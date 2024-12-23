d7b30f5962634656b23ed91177a33ab9035c6b03
Jean Pierre Ribeiro <jeanpierreribeiro.br@gmail.com>
2024-12-23 01:02:07 -0300

    Initial commit from your app

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>My Simple Blog</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        <NuxtLink :to="`/posts/${post.id}`">{{ post.title }}</NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+const posts = [
+  { id: 1, title: 'First Blog Post', content: 'This is the content of the first blog post.' },
+  { id: 2, title: 'Second Blog Post', content: 'This is the content of the second blog post.' },
+  { id: 3, title: 'Third Blog Post', content: 'This is the content of the third blog post.' },
+];
+</script>
---
+++ pages/posts/[id].vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>{{ post.title }}</h1>
+    <p>{{ post.content }}</p>
+    <NuxtLink to="/">Back to Home</NuxtLink>
+  </div>
+</template>
+
+<script setup>
+const route = useRoute();
+const posts = [
+  { id: 1, title: 'First Blog Post', content: 'This is the content of the first blog post.' },
+  { id: 2, title: 'Second Blog Post', content: 'This is the content of the second blog post.' },
+  { id: 3, title: 'Third Blog Post', content: 'This is the content of the third blog post.' },
+];
+const post = posts.find(p => p.id === parseInt(route.params.id));
+</script>
